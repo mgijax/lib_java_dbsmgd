779b6b5de8a563941a7895a1039e0b91300e4953
mbw <>
2003-09-18 18:32:54 +0000

    original version

---
+++ java/org/jax/mgi/dbs/mgd/dao/UserInterpreter.java
@@ -0,0 +1,31 @@
+package org.jax.mgi.dbs.mgd.dao;
+
+import org.jax.mgi.shr.dbutils.RowDataInterpreter;
+import org.jax.mgi.shr.dbutils.RowReference;
+import org.jax.mgi.shr.dbutils.DBException;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: </p>
+ * @author not attributable
+ * @version 1.0
+ */
+
+public class UserInterpreter implements RowDataInterpreter
+{
+
+  public Object interpret(RowReference rowReference) throws DBException
+  {
+    UserKey key = new UserKey(rowReference.getInt("_User_key"));
+    UserState state = new UserState();
+    state.setUserStatusKey(rowReference.getInt("_UserStatus_key"));
+    state.setUserTypeKey(rowReference.getInt("_UserType_key"));
+    state.setLogin(rowReference.getString("login"));
+    state.setName(rowReference.getString("name"));
+    User user = new User(key, state);
+    return user;
+  }
+
+}
---
+++ java/org/jax/mgi/dbs/mgd/dao/UserKey.java
@@ -0,0 +1,62 @@
+package org.jax.mgi.dbs.mgd.dao;
+
+import org.jax.mgi.shr.dbutils.Table;
+import org.jax.mgi.shr.dbutils.SQLDataManager;
+import org.jax.mgi.shr.dbutils.SQLDataManagerFactory;
+import org.jax.mgi.shr.dbutils.RecordStamp_MGD;
+import org.jax.mgi.shr.dbutils.DBException;
+import org.jax.mgi.shr.config.ConfigException;
+
+/**
+ * @is a key class for the User DataInstance
+ * @has a int key value
+ * @does can get the next key value when constructing a new instance and
+ * provides an accessor method for the key value
+ * @company The Jackson Laboratory
+ * @author not attributable
+ * @version 1.0
+ */
+
+public class UserKey
+{
+  /**
+   * the key value
+   */
+  private int key;
+
+  /**
+   * the default constructor which will obtain the next key value
+   * automatically
+   * @throws ConfigException thrown if there is an error accessing the
+   * configuration file when obtaining database connection parameters
+   * @throws DBException thrown if there is an error accessing the database
+   */
+  public UserKey() throws ConfigException, DBException
+  {
+    SQLDataManager sqlMgr =
+        SQLDataManagerFactory.getShared(SQLDataManagerFactory.MGD);
+    Table userTable =
+        new Table("MGI_USER", sqlMgr, new RecordStamp_MGD());
+    int key = userTable.getNextKey();
+    this.key = key;
+  }
+
+  /**
+   * the constructor which accepts the key value
+   * @param key the key value
+   */
+  public UserKey(int key)
+  {
+    this.key = key;
+  }
+
+  /**
+   * get the key value
+   * @return the key value
+   */
+  public int getKey()
+  {
+    return key;
+  }
+
+}
