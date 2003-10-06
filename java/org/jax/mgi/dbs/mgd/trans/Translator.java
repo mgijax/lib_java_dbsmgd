80f628a75d98037849235bf2df1e6649bb32ee0d
mbw <>
2003-10-06 14:53:03 +0000

    added new inner class for looking up translationType by name

--- java/org/jax/mgi/dbs/mgd/trans/Translator.java
+++ java/org/jax/mgi/dbs/mgd/trans/Translator.java
@@ -69,9 +69,11 @@ public class Translator extends CachedLookup
    * constructor
    * @assumes nothing
    * @effects nothing
-   * @throws CacheException thrown if there is an error 
-   * @throws DBException
-   * @throws ConfigException
+   * @throws CacheException thrown if there is an error accessing cache 
+   * @throws DBException thrown if there is an error with the db
+   * @throws ConfigException thrown if there is an error accessing the 
+   * configuration
+   * @lookupException thrown if there is an error using the 
    */
   public Translator(int translationType, int cacheType)
   throws CacheException, DBException, 
@@ -352,6 +354,79 @@ public class Translator extends CachedLookup
 			return new Interpreter();
 		}
 	} 
+	
+	/**
+	 *
+	 * @is: a FullCachedLookup for caching translation types and allowing
+	 * queries on the translation name to get back the record key 
+	 * @has: a RowDataCacheStrategy of type FULL_CACHE used for creating the
+	 * cache and performing the cache lookup.
+	 * @does: provides a lookup method for looking up translation type key by
+	 * translation name within a cache.
+	 * @company: The Jackson Laboratory
+	 * @author not attributable
+	 * @version 1.0
+	 */
+	public class TranslationTypeKeyLookup extends FullCachedLookup
+	{
+
+		private static HashMap cache = new HashMap();
+
+		/**
+		 * constructor
+		 * @assumes nothing
+		 * @effects nothing
+		 * @throws CacheException thrown if there is an error
+		 * @throws DBException
+		 * @throws ConfigException
+		 */
+		public TranslationTypeKeyLookup()
+		throws CacheException, DBException, 
+					 ConfigException, KeyNotFoundException
+		{
+			super(SQLDataManagerFactory.getShared(SQLDataManagerFactory.MGD));
+			setCache(cache);
+		}
+
+		/**
+		 * lookup a MGI type for the given translation type
+		 * @param the given translation type
+		 * @return the MGI type
+		 */
+		public Integer lookup(String translationName)
+				throws LookupException
+		{
+			Object o = lookup(translationName);
+			return (Integer)o;
+		}
+
+		/**
+		 * get the sql string to use in fully intializing the cache. This method
+		 * is called by the CacheStrategy class when performing initialization.
+		 * @return the sql string to use in fully intializing the cache
+		 */
+		public String getFullInitQuery()
+		{
+			String s = "SELECT _TranslationType_key, translationType " +
+								 "FROM MGI_TranslationType";
+			return s;
+		}
+
+		/**
+		 * get the RowDataInterpreter for interpreting results from a database query.
+		 * @return the RowDataInterpreter
+		 */
+		public RowDataInterpreter getRowDataInterpreter()
+		{
+			class Interpreter implements RowDataInterpreter {
+				public Object interpret(RowReference row) throws DBException {
+				 return new KeyValue(row.getString(2), row.getInt(1));
+				}
+			}
+			return new Interpreter();
+		}
+	}
+
 }
 
 
