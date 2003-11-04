3deb58335124a6a8df1bae08bb0501b1ef4708ea
mbw <>
2003-11-04 17:04:38 +0000

    modified to reflect changes in target code

--- test/org/jax/mgi/dbs/mgd/lookup/TestStrainKeyLookup.java
+++ test/org/jax/mgi/dbs/mgd/lookup/TestStrainKeyLookup.java
@@ -10,6 +10,7 @@ public class TestStrainKeyLookup
     extends TestCase {
 
   private StrainKeyLookup strainKeyLookup = null;
+  private SQLDataManager sqlMgr = null;
 
   public TestStrainKeyLookup(String name) {
     super(name);
@@ -18,25 +19,43 @@ public class TestStrainKeyLookup
   protected void setUp() throws Exception {
     super.setUp();
     strainKeyLookup = new StrainKeyLookup();
+    sqlMgr = new SQLDataManager();
   }
 
   protected void tearDown() throws Exception {
     strainKeyLookup = null;
+    sqlMgr = null;
     super.tearDown();
   }
 
   public void testLookup() throws CacheException, DBException,
       TranslationException, ConfigException {
+    String sql = "SELECT _strain_key " +
+        "FROM prb_strain " +
+        "WHERE strain = 'BALB/cJ'";
+    ResultsNavigator nav = sqlMgr.executeQuery(sql);
+    nav.next();
+    RowReference row = (RowReference) nav.getCurrent();
+    Integer key = row.getInt(1);
+
     String term = "BALB/cJ";
-    Integer expectedReturn = new Integer(3);
+    Integer expectedReturn = key;
     Integer actualReturn = strainKeyLookup.lookup(term);
     assertEquals("return value", expectedReturn, actualReturn);
   }
 
   public void testTranslatedLookup() throws CacheException, DBException,
       TranslationException, ConfigException {
+    String sql = "SELECT _strain_key " +
+        "FROM prb_strain " +
+        "WHERE strain = 'BALB/cJ'";
+    ResultsNavigator nav = sqlMgr.executeQuery(sql);
+    nav.next();
+    RowReference row = (RowReference) nav.getCurrent();
+    Integer key = row.getInt(1);
+
     String term = "BALB/c/J";
-    Integer expectedReturn = new Integer(3);
+    Integer expectedReturn = key;
     Integer actualReturn = strainKeyLookup.lookup(term);
     assertEquals("return value", expectedReturn, actualReturn);
   }
--- test/org/jax/mgi/dbs/mgd/lookup/TestVocabKeyLookup.java
+++ test/org/jax/mgi/dbs/mgd/lookup/TestVocabKeyLookup.java
@@ -4,11 +4,13 @@ import junit.framework.*;
 import org.jax.mgi.shr.cache.*;
 import org.jax.mgi.shr.dbutils.*;
 import org.jax.mgi.dbs.mgd.trans.*;
+import org.jax.mgi.dbs.mgd.VocabularyTypeConstants;
 import org.jax.mgi.shr.config.*;
 
 public class TestVocabKeyLookup
     extends TestCase {
   private VocabKeyLookup vocabKeyLookup = null;
+  private SQLDataManager sqlMgr = null;
 
   public TestVocabKeyLookup(String name) {
     super(name);
@@ -16,18 +18,27 @@ public class TestVocabKeyLookup
 
   protected void setUp() throws Exception {
     super.setUp();
-    vocabKeyLookup = new VocabKeyLookup();
+    vocabKeyLookup = new VocabKeyLookup(VocabularyTypeConstants.GENDER);
+    sqlMgr = new SQLDataManager();
   }
 
   protected void tearDown() throws Exception {
     vocabKeyLookup = null;
+    sqlMgr = null;
     super.tearDown();
   }
 
   public void testLookup() throws CacheException, DBException,
       TranslationException, ConfigException {
+    String sql = "SELECT _term_key " +
+                 "FROM voc_term " +
+                 "WHERE _vocab_key = 15 and term = 'Female'";
+    ResultsNavigator nav = sqlMgr.executeQuery(sql);
+    nav.next();
+    RowReference row = (RowReference)nav.getCurrent();
+    Integer key = row.getInt(1);
     String term = "Female";
-    Integer expectedReturn = new Integer(64555);
+    Integer expectedReturn = key;
     Integer actualReturn = vocabKeyLookup.lookup(term);
     assertEquals("return value", expectedReturn, actualReturn);
   }
