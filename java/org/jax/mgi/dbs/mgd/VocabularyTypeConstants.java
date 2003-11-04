36ebd0e160eb417e78f02307bff907abfd07d74e
mbw <>
2003-11-04 01:59:35 +0000

    initial version

---
+++ java/org/jax/mgi/dbs/mgd/VocabularyTypeConstants.java
@@ -0,0 +1,22 @@
+package org.jax.mgi.dbs.mgd;
+
+/**
+ * @is An object that contains constant definitions for vocabulary types.
+ * @has
+ *   <UL>
+ *   <LI> Constant definitions
+ *   </UL>
+ * @does Nothing
+ * @company The Jackson Laboratory
+ * @author mbw
+ * @version 1.0
+ */
+
+public class VocabularyTypeConstants
+{
+    // Constants
+    //
+    public static final int GENDER = 15;
+    public static final int CELLLINE = 17;
+    public static final int SEQUENCETYPE = 22;
+}
