b91ead4e4bad5fa0fdb7e6c50f8647aaef4bae98
Sharon Giannatto <Sharon.Giannatto@jax.org>
2003-11-13 20:40:03 +0000

    initial commit

---
+++ java/org/jax/mgi/dbs/mgd/MGIRefAssocTypeConstants.java
@@ -0,0 +1,56 @@
+//  $Header
+//  $Name
+
+package org.jax.mgi.dbs.mgd;
+
+/**
+ * @is An object that contains constant definitions for
+ *     MGI Reference Associationtypes.
+ * @has
+ *   <UL>
+ *   <LI> Constant definitions
+ *   </UL>
+ * @does Nothing
+ * @company The Jackson Laboratory
+ * @author sc
+ * @version 1.0
+ */
+
+public class MGIRefAssocTypeConstants {
+        // Constants
+        //
+        public static final int ORIGINAL = 1000;
+        public static final int GENERAL = 1002;
+        public static final int PRIMARY = 1003;
+        public static final int BROADCAST = 1004;
+        public static final int DO_NOT_BROADCAST = 1005;
+        public static final int PROVIDER = 1006;
+        public static final int LOAD = 1007;
+        public static final int CURATOR = 1008;
+}
+
+
+//  $Log
+
+/**************************************************************************
+*
+* Warranty Disclaimer and Copyright Notice
+*
+*  THE JACKSON LABORATORY MAKES NO REPRESENTATION ABOUT THE SUITABILITY OR
+*  ACCURACY OF THIS SOFTWARE OR DATA FOR ANY PURPOSE, AND MAKES NO WARRANTIES,
+*  EITHER EXPRESS OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR A
+*  PARTICULAR PURPOSE OR THAT THE USE OF THIS SOFTWARE OR DATA WILL NOT
+*  INFRINGE ANY THIRD PARTY PATENTS, COPYRIGHTS, TRADEMARKS, OR OTHER RIGHTS.
+*  THE SOFTWARE AND DATA ARE PROVIDED "AS IS".
+*
+*  This software and data are provided to enhance knowledge and encourage
+*  progress in the scientific community and are to be used only for research
+*  and educational purposes.  Any reproduction or use for commercial purpose
+*  is prohibited without the prior express written permission of The Jackson
+*  Laboratory.
+*
+* Copyright \251 1996, 1999, 2002, 2003 by The Jackson Laboratory
+*
+* All Rights Reserved
+*
+**************************************************************************/
