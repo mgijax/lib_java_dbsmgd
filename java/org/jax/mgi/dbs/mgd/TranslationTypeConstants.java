a9af7f5b1961b82e0ace5c59a7c0bc5773ead17b
mbw <>
2003-12-03 19:00:57 +0000

    added CELL and fixed LIBRARY

--- java/org/jax/mgi/dbs/mgd/TranslationTypeConstants.java
+++ java/org/jax/mgi/dbs/mgd/TranslationTypeConstants.java
@@ -20,6 +20,7 @@ public class TranslationTypeConstants
     public static final int ORGANISM = 1001;
     public static final int SEQUENCETYPE = 1002;
     public static final int TISSUE = 1003;
-    public static final int LIBRARY = 1004;
+    public static final int CELL = 1004;
+    public static final int LIBRARY = 1005;
     public static final int STRAIN = 1006;
 }
