8cd72a03a78ad18c7107d4d982c8976b908f2bce
mbw <>
2004-02-19 16:56:41 +0000

    initial version

---
+++ java/org/jax/mgi/dbs/mgd/SegmentTypeConstants.java
@@ -0,0 +1,26 @@
+package org.jax.mgi.dbs.mgd;
+
+/**
+ * @is An object that contains constant definitions for segment types.
+ * @has
+ *   <UL>
+ *   <LI> Constant definitions
+ *   </UL>
+ * @does Nothing
+ * @company The Jackson Laboratory
+ * @author mbw
+ * @version 1.0
+ */
+
+public class SegmentTypeConstants
+{
+    // Constants
+    //
+    public static final int CDNA = 63468;
+    public static final int DNA_CONSTRUCT = 63469;
+    public static final int GENOMIC = 63470;
+    public static final int MITOCHONDRIAL = 63471;
+    public static final int OLIGO = 63472;
+    public static final int PRIMER = 63473;
+    public static final int NOT_SPECIFIED = 63474;
+}
