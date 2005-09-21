24992564070ad6f3139653d3fcb520650a51201c
Sharon Giannatto <Sharon.Giannatto@jax.org>
2005-09-21 14:35:21 +0000

    initial commit tr1560 SNP

---
+++ java/org/jax/mgi/dbs/mgd/MGISetConstants.java
@@ -0,0 +1,20 @@
+package org.jax.mgi.dbs.mgd;
+
+/**
+ * @is An object that contains constant definitions for MGI_Set.
+ * @has
+ *   <UL>
+ *   <LI> Constant definitions
+ *   </UL>
+ * @does Nothing
+ * @company The Jackson Laboratory
+ * @author dbm
+ * @version 1.0
+ */
+
+public class MGISetConstants
+{
+    // Constants
+    //
+    public static final int SNPSTRAIN = 1023;
+}
