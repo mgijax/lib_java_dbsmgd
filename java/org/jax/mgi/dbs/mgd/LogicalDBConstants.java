72b1401eb7bf8330c89bf7d85f57a06fa9b84610
Sharon Giannatto <Sharon.Giannatto@jax.org>
2006-04-28 12:09:02 +0000

    lib_java_dbsmgd-3-5-0-1

--- java/org/jax/mgi/dbs/mgd/LogicalDBConstants.java
+++ java/org/jax/mgi/dbs/mgd/LogicalDBConstants.java
@@ -1,15 +1,3 @@
-//  $Header$
-//  $Header$
-//  $Name$
-//  $Header$
-//  $Header$
-//  $Header$
-//  $Header$
-//  $Name$
-//  $Header$
-//  $Header$
-//  $Name$
-
 package org.jax.mgi.dbs.mgd;
 
 /**
@@ -43,6 +31,7 @@ public class LogicalDBConstants
     public static final int SUBSNP = 74;
     public static final int SUBMITTERSNP = 75;
     public static final int SNPPOPULATION = 76;
+    public static final int HOMOLOGENE = 81;
 
     public static final int RIKEN = 51;
     public static final int NIA15K = 46;
@@ -51,76 +40,3 @@ public class LogicalDBConstants
     public static final int PUBMED = 29;
     public static final int MEDLINE = 7;
 }
-
-//  $Log$
-//  Revision 1.7  2005/12/02 16:04:30  pf
-//  3.41 maint7119 branch merge to trunk
-//
-//  Revision 1.6  2005/10/12 18:13:08  jsb
-//  lib_java_dbsmgd-3-4-0-0
-//
-//  Revision 1.5.6.1  2005/09/21 19:54:35  sc
-//  merge of snp and tr5972 branch to tr7081 branch
-//
-//  Revision 1.5.8.1  2005/11/09 00:08:25  jw
-//  lib_java_dbsmgd-3-4-1-0
-//
-//  Revision 1.6  2005/10/12 18:13:08  jsb
-//  lib_java_dbsmgd-3-4-0-0
-//
-//  Revision 1.5.6.1  2005/09/21 19:54:35  sc
-//  merge of snp and tr5972 branch to tr7081 branch
-//
-//  Revision 1.5  2005/08/05 17:51:26  mbw
-//  merged code from branch lib_java_dbsmgd-tr6046-1
-//
-//  Revision 1.4.8.1  2005/08/02 17:49:19  mbw
-//  added entry for ENTREZ_GENE
-//
-//  Revision 1.4.18.1  2005/08/30 15:24:58  sc
-//  TR1560 SNP
-//
-//  Revision 1.4  2004/12/17 17:58:29  sc
-//  tr6365
-//
-//  Revision 1.2.6.1  2004/12/16 15:44:57  sc
-//  added NIA15K, NIA7_4K, RIKEN
-//
-//  Revision 1.3  2004/10/12 12:41:13  sc
-//  TR5395 added NCBI_GENE ENSEMBL_GENE
-//
-//  Revision 1.2  2004/02/04 19:41:28  mbw
-//  merged jsam branch to the trunk
-//
-//  Revision 1.1.2.2  2004/02/02 20:02:13  sc
-//  added constants for all the sequence providers
-//
-//  Revision 1.1.2.1  2003/11/13 20:37:26  sc
-//  added PUBMED and MEDLINE constants
-//
-//  Revision 1.1  2003/09/18 14:25:17  dbm
-//  Initial version
-//
-//
-/**************************************************************************
-*
-* Warranty Disclaimer and Copyright Notice
-*
-*  THE JACKSON LABORATORY MAKES NO REPRESENTATION ABOUT THE SUITABILITY OR
-*  ACCURACY OF THIS SOFTWARE OR DATA FOR ANY PURPOSE, AND MAKES NO WARRANTIES,
-*  EITHER EXPRESS OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR A
-*  PARTICULAR PURPOSE OR THAT THE USE OF THIS SOFTWARE OR DATA WILL NOT
-*  INFRINGE ANY THIRD PARTY PATENTS, COPYRIGHTS, TRADEMARKS, OR OTHER RIGHTS.
-*  THE SOFTWARE AND DATA ARE PROVIDED "AS IS".
-*
-*  This software and data are provided to enhance knowledge and encourage
-*  progress in the scientific community and are to be used only for research
-*  and educational purposes.  Any reproduction or use for commercial purpose
-*  is prohibited without the prior express written permission of The Jackson
-*  Laboratory.
-*
-* Copyright \251 1996, 1999, 2002, 2003 by The Jackson Laboratory
-*
-* All Rights Reserved
-*
-**************************************************************************/
