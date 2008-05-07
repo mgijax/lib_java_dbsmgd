af2cba8c37e9ee63251ab138f13b3a5de56be42d
CVS 2 Git Conversion <mgiadmin@jax.org>
2008-05-07 15:37:20 +0000

    This commit was manufactured by cvs2svn to create tag 'lib_java_dbsmgd- tr7493-BP'.
    Sprout from master 2008-05-07 15:37:18 UTC Sharon Giannatto <Sharon.Giannatto@jax.org> 'lib_java_dbsmgd-4-0-0-1'
    Cherrypick from master 2008-01-29 12:36:25 UTC Sharon Giannatto <Sharon.Giannatto@jax.org> 'lib_java_dbsmgd-tr7493-BP':
        HISTORY
        java/org/jax/mgi/dbs/mgd/lookup/CoordMapCollectionKeyLookup.java

--- java/org/jax/mgi/dbs/mgd/lookup/CoordMapCollectionKeyLookup.java
+++ java/org/jax/mgi/dbs/mgd/lookup/CoordMapCollectionKeyLookup.java
@@ -1,4 +1,5 @@
-
+//  $Header
+//  $Name
 package org.jax.mgi.dbs.mgd.lookup;
 
 import java.util.HashMap;
@@ -54,8 +55,8 @@ public class CoordMapCollectionKeyLookup extends FullCachedLookup{
      * @throws KeyNotFoundException thrown if the key is not found
      */
     public Integer lookup (String mapCollection)
-        throws DBException, CacheException {
-        return (Integer)super.lookupNullsOk(mapCollection);
+        throws KeyNotFoundException, DBException, CacheException {
+        return (Integer)super.lookup(mapCollection);
     }
 
     /**
@@ -90,6 +91,29 @@ public class CoordMapCollectionKeyLookup extends FullCachedLookup{
         return new Interpreter();
     }
 }
+// $Log
+/**************************************************************************
+*
+* Warranty Disclaimer and Copyright Notice
+*
+*  THE JACKSON LABORATORY MAKES NO REPRESENTATION ABOUT THE SUITABILITY OR
+*  ACCURACY OF THIS SOFTWARE OR DATA FOR ANY PURPOSE, AND MAKES NO WARRANTIES,
+*  EITHER EXPRESS OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR A
+*  PARTICULAR PURPOSE OR THAT THE USE OF THIS SOFTWARE OR DATA WILL NOT
+*  INFRINGE ANY THIRD PARTY PATENTS, COPYRIGHTS, TRADEMARKS, OR OTHER RIGHTS.
+*  THE SOFTWARE AND DATA ARE PROVIDED "AS IS".
+*
+*  This software and data are provided to enhance knowledge and encourage
+*  progress in the scientific community and are to be used only for research
+*  and educational purposes.  Any reproduction or use for commercial purpose
+*  is prohibited without the prior express written permission of The Jackson
+*  Laboratory.
+*
+* Copyright \251 1996, 1999, 2002, 2003 by The Jackson Laboratory
+*
+* All Rights Reserved
+*
+**************************************************************************/
 
 
 
