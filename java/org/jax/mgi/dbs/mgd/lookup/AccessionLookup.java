42a7035fb657a003a8a884e4a40e769d44686510
Sharon Giannatto <Sharon.Giannatto@jax.org>
2009-07-14 12:36:20 +0000

    lib_java_dbsmgd-gtlf-7

--- java/org/jax/mgi/dbs/mgd/lookup/AccessionLookup.java
+++ java/org/jax/mgi/dbs/mgd/lookup/AccessionLookup.java
@@ -10,7 +10,6 @@ import org.jax.mgi.shr.cache.CacheConstants;
 import org.jax.mgi.shr.cache.KeyValue;
 import org.jax.mgi.shr.cache.CacheException;
 import org.jax.mgi.shr.config.ConfigException;
-import org.jax.mgi.shr.cache.KeyNotFoundException;
 import org.jax.mgi.shr.dbutils.DBException;
 import org.jax.mgi.shr.dbutils.SQLDataManagerFactory;
 import org.jax.mgi.dbs.SchemaConstants;
@@ -37,8 +36,6 @@ public class AccessionLookup extends CachedLookup {
 
     /**
      * Constructs an AccessionLookup object.
-     * @assumes Nothing
-     * @effects Nothing
      * @param logicalDBKey the logical db of the accession id to look up
      * @param mgiTypeKey the MGI type of the object key to return
      * @param preferred true (1)if preferred accession ids
